94808b45445f4d24dec9be50ff78f642fe547d12
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-14 23:35:41 +0600

    booking appointment modal set

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns/esm';
 import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOptions from './AppointmentOptions';
 
 const AvailableAppointments = ({ selectedDate }) => {
     const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
         fetch('appointmentOptions.json')
@@ -19,9 +21,16 @@ const AvailableAppointments = ({ selectedDate }) => {
                     appointmentOptions.map(option => <AppointmentOptions
                         key={option._id}
                         appointmentOption={option}
+                        setTreatment={setTreatment}
                     ></AppointmentOptions>)
                 }
             </div>
+            {
+                treatment &&
+                <BookingModal
+                    treatment={treatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
